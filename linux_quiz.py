f25e4400c8c9979f26b3ddbd8d59658c59c5cde0
DESKTOP-MJQVKHB\user1 <pbsokok7@gmail.com>
2026-07-24 16:45:40 +0900

    완성본 v1

--- linux_quiz.py
+++ linux_quiz.py
@@ -48,6 +48,8 @@ def printquiz(x):
             print(f"정답은{good5} 이였습니다!")
             return False
 
+
+
 # 퀴즈목록
 quiz_list = []
 quiz_list.append("건이는 어떤 파일에 다른사람에게도 모든 권한을 주고싶어한다 터미널에 어떤 명령어를 써야할까?")
@@ -60,7 +62,6 @@ quiz_list.append("건이는 전에 자신이 어떤 명령어를 써왔는지 �
 good4 = ["history","History","HISTORY"] #4번 문제
 quiz_list.append("건이는 메모장에 쓰고싶은걸 다 쓴다음 저장하고 나가고 싶어한다 어떤 명령어를 써야할까?")
 good5 = [":wq", ":WQ", "Wq"]#5번 문제
-
 
 
 
